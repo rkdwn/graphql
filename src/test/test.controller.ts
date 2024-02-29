16de351ba9f5ceb24f92f5f6f897b1173ec19648
rkdwn <kj2693119@gmail.com>
2024-02-29 16:43:24 +0900

    feat: PDF 생성 테스트 코드 추가

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,10 +1,14 @@
 import { StorageService } from "@/common/storage/storage.service";
 import { Controller, Get, Inject, StreamableFile } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
+import { TestService } from "./test.service";
 
 @Controller("/test")
 export class TestController {
-  constructor(private readonly storageService: StorageService) {}
+  constructor(
+    private readonly storageService: StorageService,
+    private readonly testService: TestService
+  ) {}
 
   @Get("/")
   async getConfigFile(req: Request, res: Response, next: NextFunction) {
@@ -13,5 +17,11 @@ export class TestController {
       "config"
     );
     return new StreamableFile(resultStream);
+  }
+
+  @Get("/pdf-test")
+  async generatePDF(req: Request, res: Response, next: NextFunction) {
+    const result = await this.testService.captureTest();
+    return res.status(200).send(result);
   }
 }
